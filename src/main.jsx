9f08485202cdb77bcea846834fa516e673099280
Meet Naik <meet.naikk@gmail.com>
2026-02-03 23:32:14 -0500

    Add navigation links between Login and Signup pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 //import App from './App.jsx'
-import Login from './pages/Login.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
   </StrictMode>,
 )
